6f1a5a1cfa38a8a6a5fef3182ed8050b4f8f90f3
Khashayar Hajnabi <Khashayar.hajnabi@gmail.com>
2022-04-18 15:27:34 +0430

    Doing the second part.

--- script.js
+++ script.js
@@ -12,4 +12,12 @@ function btnClick() {
   const age = document.getElementById("age");
   radioChecked();
   alert(name.value + " " + lastname.value + " " + age.value + " " + sex);
+  let numA = prompt("Please enter a numbre,A");
+  let a = numA;
+  let numB = prompt("Please enter a numbre,B");
+  let b = numB;
+  alert("A =" + a + " " + "B =" + b);
+  b = numA;
+  a = numB;
+  alert("A =" + a + " " + "B =" + b);
 }
