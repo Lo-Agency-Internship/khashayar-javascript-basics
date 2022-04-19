265bfd007da1ad338f7bec0632711424bc477bf8
Khashayar Hajnabi <Khashayar.hajnabi@gmail.com>
2022-04-19 09:37:29 +0430

    Using prompt to showing info on page.

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
-let sex;
-function radioChecked() {
-  var radios = document.getElementsByName("sex");
-
-  for (const radio of radios) {
-    if (radio.checked) sex = radio.value;
-  }
-}
-function btnClick() {
-  const name = document.getElementById("name");
-  const lastname = document.getElementById("lastname");
-  const age = document.getElementById("age");
-  radioChecked();
-  alert(name.value + " " + lastname.value + " " + age.value + " " + sex);
-  let numA = prompt("Please enter a numbre,A");
-  let a = numA;
-  let numB = prompt("Please enter a numbre,B");
-  let b = numB;
-  alert("A =" + a + " " + "B =" + b);
-  b = numA;
-  a = numB;
-  alert("A =" + a + " " + "B =" + b);
+info();
+function info() {
+  const name = prompt("please enter your name:");
+  const lastname = prompt("please enter your lastname:");
+  const age = prompt("please enter your age:");
+  const gender = prompt("please enter your gender:");
+  const person = `  Name: ${name}
+  Lastname: ${lastname}
+  Sex: ${gender}
+  Age: ${age}`;
+  alert(person);
+  let root = document.getElementById("root");
+  root.append(person);
 }
